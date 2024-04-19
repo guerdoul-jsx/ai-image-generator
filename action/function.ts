0144bbd32d6dedfd3ec6f0a81a5f7d63b410bb4e
= <=>
2024-04-19 12:44:20 +0100

    update auth key

--- action/function.ts
+++ action/function.ts
@@ -4,7 +4,7 @@ import Replicate from "replicate";
 
 const replicate = new Replicate({
   // get your token from https://replicate.com/account
-  auth: "r8_7JV3FqUMfzzjcY91HqurFkzczFbH0Rp2QueyZ", // defaults to process.env.REPLICATE_API_TOKEN
+  auth: process.env.REPLICATE_API_TOKEN, // defaults to process.env.REPLICATE_API_TOKEN
 });
 
 export async function getListCollections() {
